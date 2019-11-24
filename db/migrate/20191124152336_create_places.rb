ef37bf7f5ccfa8d47c525211f68d3691e73b79ee
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-24 14:55:52 -0300

    commit

---
+++ db/migrate/20191124152336_create_places.rb
@@ -0,0 +1,17 @@
+class CreatePlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :zipcode
+      t.float :latitude
+      t.float :logitude
+      t.integer :capacity
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_045047) do
+ActiveRecord::Schema.define(version: 2019_11_24_152336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "zipcode"
+    t.float "latitude"
+    t.float "logitude"
+    t.integer "capacity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
