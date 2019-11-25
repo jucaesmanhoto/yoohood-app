cd7c2da7f5440423267d5fc82ab1f94e577e62ca
Vinicius Alvim <vini0191@gmail.com>
2019-11-25 20:49:51 -0300

    fixed version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :invites
+  has_many :benefits, through: :trades
+  has_many :trades
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying events...'
+Event.destroy_all
+
+puts 'Destroying users...'
+User.destroy_all
+
+puts 'Creating fake Users...'
+20.times do
+  User.create!(name: Faker::Name.name,
+               email: Faker::Internet.email,
+               password: 'password')
+end
+
+puts 'Creating fake places...'
+20.times do
+  Place.create!(name: Faker::Name.name,
+                address: Faker::Address.street_address)
+end
+
+puts 'Creating fake Events...'
+100.times do
+  Event.create!(title: Faker::Name.name,
+                description: "Solomun is back! Don't miss his next set in Sao Paulo.",
+                start_time: Faker::Time.forward(days: 23, period: :evening),
+                user: User.all.sample,
+                place: Place.all.sample)
+end
