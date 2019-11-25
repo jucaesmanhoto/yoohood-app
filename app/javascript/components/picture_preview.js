83fc20b37b3192c0bfb9615bd133e827e3f10d03
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-25 14:34:06 -0300

    new branch

--- app/javascript/components/picture_preview.js
+++ app/javascript/components/picture_preview.js
@@ -1,10 +1,36 @@
-const pictureFrame = document.querySelector('.frame')
-const pictureCache = document.querySelector('#place_front_picture_cache')
-const initPicturePreview = () => {
-  if (pictureFrame) {
-    console.log(pictureCache);
+// const pictureFrame = document.querySelector('.frame')
+// const pictureCache = document.querySelector('#place_front_picture_cache')
+// const initPicturePreview = () => {
+//   if (pictureFrame) {
+//     console.log(pictureCache);
 
+//   }
+// }
+
+// export { initPicturePreview };
+
+// app/javascript/components/photo_preview.js
+const previewImageOnFileSelect = () => {
+  // we select the photo input
+  const input = document.querySelector('.photo-input');
+  if (input) {
+    // we add a listener to know when a new picture is uploaded
+    input.addEventListener('change', () => {
+      // we call the displayPreview function (who retrieve the image url and display it)
+      displayPreview(input);
+    })
   }
 }
 
-export { initPicturePreview };
+const displayPreview = (input) => {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      document.getElementById('photo-preview').src = event.currentTarget.result;
+    }
+    reader.readAsDataURL(input.files[0])
+    document.getElementById('photo-preview').classList.remove('hidden');
+  }
+}
+
+export { previewImageOnFileSelect };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import { initPicturePreview } from '../components/picture_preview';
+import { previewImageOnFileSelect } from '../components/picture_preview';
 import { initMapbox } from '../plugins/init_mapbox';
 
 
 initMapbox();
-initPicturePreview();
+previewImageOnFileSelect();
