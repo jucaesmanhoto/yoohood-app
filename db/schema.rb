62feabe0d6e7f0d5e2c20a31355401748be0e981
Vinicius Alvim <vini0191@gmail.com>
2019-11-25 14:21:07 -0300

    fix start_time and event_time in events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_152130) do
+ActiveRecord::Schema.define(version: 2019_11_25_171807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_152130) do
 
   create_table "events", force: :cascade do |t|
     t.boolean "featured"
-    t.string "start_time"
-    t.string "end_time"
     t.string "cover_url"
     t.string "title"
     t.string "description"
@@ -35,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_152130) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "end_time"
+    t.datetime "start_time"
     t.index ["benefit_id"], name: "index_events_on_benefit_id"
     t.index ["place_id"], name: "index_events_on_place_id"
     t.index ["user_id"], name: "index_events_on_user_id"
