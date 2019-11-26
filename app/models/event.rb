41c41e2c1804f67e0309a8610d4496565e565229
Vinicius Alvim <vini0191@gmail.com>
2019-11-26 14:00:33 -0300

    ongoing...

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,10 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :invites
 
-  # include PgSearch::Model
-  # pg_search_scope :search_by_event_and_artist,
-  #                 against: %i[title description],
-  #                 using: {
-  #                   tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  #                 }
+  include PgSearch::Model
+  pg_search_scope :search_by_event_and_artist,
+                  against: %i[title description],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -6,10 +6,10 @@ class Place < ApplicationRecord
   validates :name, :address, presence: true
   after_validation :geocode, if: :will_save_change_to_address?
 
-  # include PgSearch::Model
-  # pg_search_scope :search_by_city_and_zip_code,
-  #                 against: %i[city zip_code],
-  #                 using: {
-  #                   tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  #                 }
+  include PgSearch::Model
+  pg_search_scope :search_by_city_and_zip_code,
+                  against: %i[city zip_code],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
