f1e1ee87395106185b7587270ec56c95b663ab72
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-25 21:20:48 -0300

    flashes dismiss within 2s

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import flash from "../components/flashes"
 import { previewImageOnFileSelect } from '../components/picture_preview';
 import { initMapbox } from '../plugins/init_mapbox';
 
