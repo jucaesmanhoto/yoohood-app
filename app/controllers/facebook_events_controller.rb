6a4d0ee902ef76d9d2236b28734f75bc823e0687
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-28 20:53:36 -0300

    fix

--- app/controllers/facebook_events_controller.rb
+++ app/controllers/facebook_events_controller.rb
@@ -2,42 +2,29 @@ require 'json'
 require 'open-uri'
 
 class FacebookEventsController < ApplicationController
-
-  def pull_fb_events
-    access_token = params[:token]
-    url = "https://graph.facebook.com/v4.0/me?fields=events.limit(100){cover,description,end_time,event_times,name,place,start_time,ticket_uri,type,admins}&access_token=#{access_token}"
-    serialized = open(url).read
-    @events = JSON.parse(serialized)['events']['data'].reject { |event| Event.all.include? Event.find_by_fb_event_id(event['id']) }
-    # render :index
+  def index
+    @events = FacebookServices.new(params[:token]).pull_fb_events
   end
 
   def new
   end
 
   def create
-    @event = Event.new(
-      title: params[:event][:name],
-      description: params[:event][:description],
-      user: current_user,
-      end_time: params[:event][:end_time],
-      start_time: params[:event][:start_time],
-      cover: params[:event][:cover][:source],
-      fb_event_id: params[:event][:id]
-    )
+    @event = Event.new(event_params)
+    @event.fb_event_id = params[:fb_event_id]
+    @event.user = current_user
+    # @place = Place.new(
+    #   name: params[:event][:place][:name],
+    #   address: params[:event][:place][:location][:street],
+    #   city: params[:event][:place][:location][:city],
+    #   state: params[:event][:place][:location][:state],
+    #   country: params[:event][:place][:location][:country],
+    #   latitude: params[:event][:place][:location][:latitude],
+    #   longitude: params[:event][:place][:location][:longitude],
+    #   event: @event
+    # )
 
-    @place = Place.new(
-      name: params[:event][:place][:name],
-      address: params[:event][:place][:location][:street],
-      city: params[:event][:place][:location][:city],
-      state: params[:event][:place][:location][:state],
-      country: params[:event][:place][:location][:country],
-      latitude: params[:event][:place][:location][:latitude],
-      longitude: params[:event][:place][:location][:longitude],
-      event: @event
-    )
-
-    # debugger
-    if @event.save && @place.save
+    if @event.save
       respond_to do |format|
         format.html { redirect_to fb_events_path }
         format.js
