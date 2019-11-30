1d65fed6468016c197e20d142d559912f266d765
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-30 01:54:46 -0300

    facebook login done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_024642) do
+ActiveRecord::Schema.define(version: 2019_11_30_044242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,7 +123,6 @@ ActiveRecord::Schema.define(version: 2019_11_30_024642) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
-    t.string "fb_user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
