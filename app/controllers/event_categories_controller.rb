5eac4588ad0b694406b2b7ab6cd5e8f3de2f3f2b
Vinicius Alvim <vini0191@gmail.com>
2019-12-02 16:23:14 -0300

    Fix Update_Event button

--- app/controllers/event_categories_controller.rb
+++ app/controllers/event_categories_controller.rb
@@ -1,22 +1,34 @@
 class EventCategoriesController < ApplicationController
-  before_action :set_event, only: %i[update]
+  before_action :set_event, only: %i[update destroy]
   def create
     raise
   end
 
   def update
     # @event.categories = []
-    # params[:event][:category_ids].each do |category|
+    # params[:event][:categories].each do |category|
     #   next if category.empty?
 
     #   @event.categories << Category.find(category)
     # end
-
-    EventCategory.create(event_id: @event.id, category_id: params[:event][:categories])
-    respond_to do |format|
-      format.html { redirect_to event_path(@event) }
-      format.js
+    # raise
+    if params[:event][:categories].present? && EventCategory.find_by(event_id: @event.id, category_id: params[:event][:categories])
+      respond_to do |format|
+        format.html { redirect_to event_path(@event), alert: 'Event already categorized like this' }
+        format.js
+      end
+    else
+      EventCategory.create(event_id: @event.id, category_id: params[:event][:categories])
+      respond_to do |format|
+        format.html { redirect_to event_path(@event) }
+        format.js
+      end
     end
+  end
+
+  def destroy
+    EventCategory.find(params[:id]).destroy
+    redirect_to event_path(@event)
   end
 
   private
