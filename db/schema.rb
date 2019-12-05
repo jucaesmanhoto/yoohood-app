f1387a15dc917f46160e3abd98da153210f8470c
Vinicius Alvim <vini0191@gmail.com>
2019-12-05 09:58:45 -0300

    ongoing...

--- db/schema.rb
+++ db/schema.rb
@@ -128,8 +128,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_155320) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "name"
-    t.string "fb_user_id"
-    t.integer "points", default: 0
+    t.integer "points"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
