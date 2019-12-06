cb1d4e9bf8e58589e70a83b1d692e839aa6394ed
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-12-06 14:29:59 -0300

    fix home page comming up events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,8 @@ class PagesController < ApplicationController
     # @event_near = Event.all.select{ |event| event.places.city ==  }
     @events_recent = @events.sort_by { |event| event.created_at }.first(8)
     @events_today = @events.select { |event| Date.parse(event.start_time.to_s) == Date.today }.first(8)
-    @events_coming_up = @events.select { |event| event.start_time > DateTime.now }.sort_by { |event| event.start_time }.first(8)
-    @events_diff = @events - @events_recent - @events_coming_up - @events_today
+    @events_coming_up = (@events.select { |event| event.start_time > DateTime.now }.sort_by { |event| event.start_time } - @events_today).first(8)
+    @events_diff = @events - @events_recent - @events_coming_up
     @city = request.location.city
   end
 
