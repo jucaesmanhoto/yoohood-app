f52ab952eb98636b8ef2178d405c3df26e5b9374
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-12-07 01:23:58 -0300

    changed order of home comming up events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     @page_name = 'Find a perfect event'
     @events = Event.all.reject { |event| event.end_time.present? && event.end_time <= Time.now.getutc() }
     # @event_near = Event.all.select{ |event| event.places.city ==  }
-    @events_recent = @events.sort_by { |event| event.created_at }.first(8)
+    @events_recent = @events.sort_by { |event| event.created_at }.reverse.first(8)
     @events_today = @events.select { |event| Date.parse(event.start_time.to_s) == Date.today }.first(8)
     @events_coming_up = (@events.select { |event| event.start_time > DateTime.now }.sort_by { |event| event.start_time } - @events_today).first(8)
     @events_diff = @events - @events_recent - @events_coming_up
