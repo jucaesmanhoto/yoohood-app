5bd06c2c408e2ae2d97201de7dab81d9de40d6ef
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-12-09 12:59:58 -0300

    changed the contact mail reciver to ti@yoohood.fun

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def contact(email)
     @contact = params[:contact] # Instance variable => available in view
-    mail(from: 'ti@yoohood.fun', to: 'contactus@yoohood.fun', subject: "Contact from #{email}")
+    mail(from: 'ti@yoohood.fun', to: 'ti@yoohood.fun', subject: "Contact from #{email}")
     # This will render a view in `app/views/user_mailer`!
   end
 end
