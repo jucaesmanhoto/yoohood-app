ce1b925f04c07f8343893458522ea3f64b539560
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-12-20 03:09:03 -0300

    shuffle events in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,11 @@ class PagesController < ApplicationController
   def home
     @page_name = 'Find a perfect event'
     @events = Event.all.reject { |event| event.end_time.present? && event.end_time <= Time.now.getutc() }
-    # @event_near = Event.all.select{ |event| event.places.city ==  }
     @events_recent = @events.sort_by { |event| event.created_at }.reverse.first(8)
-    @events_today = @events.select { |event| Date.parse(event.start_time.to_s) == Date.today }
+    @events_today = @events.select { |event| Date.parse(event.start_time.to_s) == Date.today }.shuffle
     @events_coming_up = (@events.select { |event| event.start_time > DateTime.now }.sort_by { |event| event.start_time } - @events_today).first(16)
-    @events_diff = (@events - @events_recent - @events_coming_up).first(16)
+    @events_diff = (@events - @events_recent - @events_coming_up).sample(16)
     @city = request.location.city
-    @nearby_events = Place.near(@city, 10).map(&:event)
   end
 
   def members
@@ -21,7 +19,6 @@ class PagesController < ApplicationController
   end
 
   def profile
-    # @meta_title = "#{DEFAULT_META["meta_product_name"]} - #{current_user.name.split(' ').first}'s profile"
     @accepted_invites = current_user.invites.select { |invite| invite.status == 'accepted' }
   end
 
