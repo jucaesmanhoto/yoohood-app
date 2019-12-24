810d5c22a922c760c558b5374ef311f315d03996
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-12-24 00:20:56 -0300

    i18n in form

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -4,10 +4,6 @@ class Invite < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  # validates :event, uniqueness: { scope: :guest_email,
-  #   message: "You've already invited this person."
-  # }
-
   def send_invite_mail(email)
     InviteMailer.with(invite: self).invitation.deliver_now
   end
